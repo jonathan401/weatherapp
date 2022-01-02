19626b789e5d66463b589e2545b5d823b9df31d1
jonathan401 <jonathangomina5049@gmail.com>
2022-01-02 16:16:02 +0100

    add loader

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('.change-location');
 const details = document.querySelector('.details');
 const card = document.querySelector('.card');
+const loader = document.querySelector('.card .preload');
 const time = document.querySelector('.card-img img');
 const icon = document.querySelector('.card-icon img');
 
@@ -12,32 +13,36 @@ const displayError = errMessage => {
 // updateUi function to update the dom when the data arrives
 const updateUI = (data) => {
     const { current, location} = data;
+    // setting the icon image
+    const iconSrc = `${current.condition.icon}`;
+    icon.setAttribute('src', iconSrc);
+
+// setting the image of the card to the time of the request
+    let timeSrc = current.is_day ? 'images/day.svg' : 'images/night.svg';
+    time.setAttribute('src', timeSrc); 
     details.innerHTML = `
         <h2>${location.name}</h2>
         <h2>${location.country}</h2>
         <p>${current.condition.text}</p>
         <p>${current.temp_c}&deg;c</p>
     `;
-
-    // setting the icon image
-    const iconSrc = `${current.condition.icon}`;
-    card-icon.setAttribute('src', iconSrc);
-
-// setting the image of the card to the time of the request
-    let timeSrc = current.is_day ? 'images/day.svg' : 'images/night.svg';
-    time.setAttribute('src', timeSrc); 
-
+    
+    loader.classList.add('hide');
+    /* after the data has been gotten from the api, the loader is given a class of 'hide' which gives the 
+    element a display of none.*/    
     if(card.classList.contains('hide')){
         card.classList.remove('hide');
     }
-    console.log(data);
+}
 
-};
-
+// handling the user input
 form.addEventListener('submit', e => {
     e.preventDefault();
     const cityName = form.city.value.trim();
     form.reset();
+    loader.classList.remove('hide');
+    /* immediately the user has submitted the form, then the loader is shown to make the user wait 
+    for the request to be processed this is done by removing the class that hides the preloader*/
     if(!cityName.length){
         displayError('You must enter a city name!');
     } else {
